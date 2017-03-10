ae9985f897c2ef9d32a2530a445319b9c3467cc0
Katrin Hansel <k.hansel@qmul.ac.uk>
2017-03-10 19:14:02 +0000

    ..

---
+++ AWSenseWatch/AWSenseWatch/AWSenseShared/SensorSettings/AccelerometerSensorSettings.swift
@@ -0,0 +1,14 @@
+//
+//  AccelerometerSensorSettings.swift
+//  AWSenseWatch
+//
+//  Created by Katrin Hansel on 10/03/2017.
+//  Copyright © 2017 Katrin Haensel. All rights reserved.
+//
+
+import Foundation
+
+public class AWSRawAccelerometerConfiguration{
+    
+    
+}
