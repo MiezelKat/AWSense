a305ca0c1937c684362e9ea10077e2dc21440cd2
Katrin Haensel <k.hansel@qmul.ac.uk>
2017-03-13 15:33:21 +0000

    ...

--- AWSenseWatchTest/AWSenseWatchTest WatchKit Extension/ExtensionDelegate.swift
+++ AWSenseWatchTest/AWSenseWatchTest WatchKit Extension/ExtensionDelegate.swift
@@ -19,8 +19,13 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
     }
 
     func applicationWillResignActive() {
+        print("will resign")
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, etc.
+    }
+    
+    func applicationDidEnterBackground() {
+        print("enter background")
     }
 
     func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
